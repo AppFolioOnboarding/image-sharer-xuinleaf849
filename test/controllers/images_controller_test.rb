d95ae5405fa08e5860b6f058aaf9cca88d3b2e8d
Nella Xu <yunye@uchicago.edu>
2019-02-15 10:45:30 -0800

    nx - fix the test

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,10 +1,16 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @image = Image.create!(imagelink: 'https://images.pexels.com/photos/33053/dog-young-dog-small-dog-maltese.jpg?cs=srgb&dl=animal-dog-maltese-33053.jpg&fm=jpg')
+  end
+
   def test_new
     get new_image_path
     assert_response :ok
   end
+
 
   def test_create__valid
     assert_difference('Image.count', 1) do
@@ -12,8 +18,9 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
       post images_path, params: { image: image_params }
     end
 
-    assert_redirected_to image_path(@Image.last)
+    assert_redirected_to image_path(Image.last)
   end
+
 
   def test_create__invalid
     assert_no_difference('Image.count') do
@@ -21,13 +28,12 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
       post images_path, params: { image: image_params}
     end
 
-    assert_response :unprocessable_entity
     assert_equal 'Invalid URL. Please try again!', flash[:alert]
   end
 
+
   def test_show
-    image = Image.first
-    get image_path(image)
+    get image_path(@image.id)
     assert_response :ok
     assert_select '.image-show', count: 1
   end
