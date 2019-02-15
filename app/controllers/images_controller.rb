5010f8ec1ca559362d4f7345bd9b7375fdd85121
Nella Xu <yunye@uchicago.edu>
2019-02-15 00:28:41 -0800

    nx - add validation gem, implement controller and its tests

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,26 +1,22 @@
 class ImagesController < ApplicationController
 
-  # def index
-  # end
-
-
   def new
     @image = Image.new
   end
 
-
   def create
-    @image = Image.new(params.require(:image).permit(:url))
+    @image = Image.new(params.require(:image).permit(:imagelink))
 
     if @image.save
-      redirect_to image_path(@image)
+      redirect_to @image
     else
+      flash[:alert] = 'Invalid URL. Please try again!'
       render :new
     end
   end
 
   def show
-    @image_url = Image.find(params[:id]).url
+    @image_url = Image.find(params[:id]).imagelink
   end
 
 end
--- db/migrate/20190214224646_change_model_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeModelName < ActiveRecord::Migration[5.2]
-  def change
-    rename_table :images, :image
-  end
-end
