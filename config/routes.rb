9d7f925fb4fd4e01ee219851aac7c7d3a88246f6
Nella Xu <yunye@uchicago.edu>
2019-03-06 00:17:14 -0800

    nx - add edit and update actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'images#index'
-  resources :images, only: %i[index new create show destroy]
+  resources :images
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#home'
 end
